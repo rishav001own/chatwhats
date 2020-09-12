f3018747089fec6d2e9995426ee45d65d6bd3a79
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-09-12 15:36:00 +0530

    frontend v3

--- chatwhatsfront/src/Chat.js
+++ chatwhatsfront/src/Chat.js
@@ -29,18 +29,12 @@ const Chat = ({ messages }) => {
             </div>
             <div className="chat__body">
             {messages.map((message)=>(
-                <p className={`chat__message ${message.recevied && "chat__receiver"}`}>
+                <p className={`chat__message ${message.recevied && "chat__reciever"}`}>
                     <span className="chat__name">{message.name}</span>
                         {message.message}
                     <span className="chat__timestamp">{message.timestamp}</span>
                 </p>
             ))}
-                
-                <p className="chat__message chat__receiver">
-                    <span className="chat__name">rishava</span>
-                        message...... 
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
             </div>
             <div className="chat__footer">
                 <InsertEmoticonIcon/>
